82908d3d2ff62b09e4354636a972b644c7a0035c
Krishna Satyanarayana <krishna@secretmission.io>
2021-09-06 22:51:25 -0400

    Create a production DB connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 
 import { ApolloServer } from 'apollo-server';
+import * as PostgresConnectionStringParser from 'pg-connection-string';
 import { env } from 'process';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 import { registerAllEntities } from './entity';
 import { ALL_RESOLVERS } from './resolvers';
@@ -11,8 +13,35 @@ import { ALL_RESOLVERS } from './resolvers';
 // Set up the DB connection and Apollo server
 (async (): Promise<void> => {
   try {
-    const connectionName = env.DB_CONNECTION_NAME ?? 'default';
-    global.connection = await createConnection(connectionName);
+    const databaseUrl = process.env.DATABASE_URL;
+    if (databaseUrl) {
+      const connectionOptions =
+        PostgresConnectionStringParser.parse(databaseUrl);
+      const postgresConnectionOptions: PostgresConnectionOptions = {
+        type: 'postgres',
+        name: 'production',
+        host: connectionOptions.host ?? undefined,
+        port: connectionOptions.port
+          ? Number.parseInt(connectionOptions.port)
+          : undefined,
+        username: connectionOptions.user,
+        password: connectionOptions.password,
+        database: connectionOptions.database ?? undefined,
+        synchronize: false,
+        logging: false,
+        entities: ['entity/**/*.ts', 'entity/**/*.js'],
+        migrations: ['migration/**/*.ts', 'migration/**/*.js'],
+        subscribers: ['subscriber/**/*.ts', 'subscriber/**/*.js'],
+        extra: {
+          ssl: true,
+        },
+      };
+      global.connection = await createConnection(postgresConnectionOptions);
+    } else {
+      const connectionName = env.DB_CONNECTION_NAME ?? 'default';
+      global.connection = await createConnection(connectionName);
+    }
+
     registerAllEntities(global.connection);
 
     const schema = await buildSchema({
