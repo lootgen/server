752da8f75311726bd830252742e47228c07d1046
Krishna Satyanarayana <krishna@secretmission.io>
2021-09-19 13:47:55 -0400

    Convert SVG to png for image download

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,48 @@
 import * as core from 'express-serve-static-core';
+import svg2img from 'svg2img';
+
+import { ILootBag } from './entity/interfaces/loot_bag_interface';
+import { LootBag } from './entity/loot_bag';
+
+const createSVGForLoot = (lootBag: ILootBag): string => {
+  const svgStrings = [
+    '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+    '<defs><style type="text/css">@import url("https://fonts.googleapis.com/css?family=EB+Garamond");</style></defs>',
+    '<style>.base { fill: white; font-family: "EB Garamond", serif; font-size: 24px; }</style><rect width="100%" height="100%" fill="black" />',
+  ];
+  lootBag.items.forEach((lootItem, index) => {
+    svgStrings.push(`<text x="24" y="${(index + 1) * 31}" class="base">`);
+    svgStrings.push(lootItem.item.name);
+    svgStrings.push('</text>');
+  });
+  svgStrings.push('</svg>');
+  return svgStrings.join('');
+};
 
 const registerExpressAPI = (app: core.Express): void => {
-  app.get('/image/:lootId', (req, res) => {
-    console.log(req.params.lootId);
-    res.send('hello world');
+  app.get('/loot/:id/image.png', async (req, res) => {
+    try {
+      const lootBag = await LootBag.findOneOrFail(req.params.id);
+      const svg = createSVGForLoot(lootBag);
+      svg2img(
+        svg,
+        {
+          width: 1024,
+          height: 1024,
+          quality: 100,
+        },
+        (error, buffer) => {
+          res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'image/png',
+            'Content-Length': buffer.length,
+          });
+          res.end(buffer);
+        }
+      );
+    } catch (error) {
+      res.sendStatus(404);
+    }
   });
 };
 
