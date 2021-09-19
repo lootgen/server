5d1d6a3ec0c22edd4f28bd128c5ed5accca90263
Krishna Satyanarayana <krishna@secretmission.io>
2021-09-19 14:40:38 -0400

    Allow all origins

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ import registerExpressAPI from './api';
 
     const app = express();
     app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', 'https://www.lootgen.party');
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, OPTIONS, PUT, PATCH, DELETE'
