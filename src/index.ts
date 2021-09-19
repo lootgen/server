138a5c0859bb98443704bddd2932f97a6497a649
Krishna Satyanarayana <krishna@secretmission.io>
2021-09-19 14:49:14 -0400

    Make this work!

--- src/index.ts
+++ src/index.ts
@@ -53,10 +53,8 @@ import registerExpressAPI from './api';
     const app = express();
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-      );
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
       next();
     });
 
